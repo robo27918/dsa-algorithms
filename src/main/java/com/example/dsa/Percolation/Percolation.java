37e1d8cd57c651503757ad66852c61904c13b31c
robo27918 <roberttoribio04@gmail.com>
2026-02-15 10:09:35 -0800

    adds method for mapping (r,c) to id

--- src/main/java/com/example/dsa/Percolation/Percolation.java
+++ src/main/java/com/example/dsa/Percolation/Percolation.java
@@ -4,26 +4,34 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     //n*n grid where 1= blocked 0 = open
-    private int [][] grid; 
+    private boolean [][] grid; // True means open, False means blocked
+    private int N ;
+    private WeightedQuickUnionUF UF;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
-        for(int i =0; i < n;i++){
-            for (int j = 0; j < n;j++){
-                this.grid[i][j] = 1;
-            }
+        this.N = n;
+        // create the WeightedQuickFind and set it as an attribute
+        UF = new WeightedQuickUnionUF(n*n);
+        grid = new boolean [n][n];
+    }   
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){
+        if (!isOpen(row,col)){
+            this.grid[row][col] = true;
         }
     }
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col){}
-
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){return this.grid[row][col] ==0;}
+    public boolean isOpen(int row, int col){return this.grid[row][col];}
 
-    // is the site (row, col) full? where full = blocked
-    public boolean isFull(int row, int col){return this.grid[row][col];}
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){
+        
+    }
 
     // returns the number of open sites
+    //
     public int numberOfOpenSites(){
             int count = 0;
             // brute force: loop through the grid and see what is open
@@ -36,7 +44,21 @@ public class Percolation {
     }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates(){
+        // start from some (i,j) in the top and use
+        // use find to check that the elements
+        // at the top have the same root 
+        //
+        //scan the top 
+        for (int j = 0; i <N;i++){
+            if(this.grid[0][j])
+        }
+        
+        use UnionFind()
+    }
+    //method for mapping row and col to id
+    private int getId(int row,int col){ return this.N * (row -1) + col;}
+
 
     // test client (optional)
     public static void main(String[] args)
